bcc76684d74788aa153042b4395cc36a4638a900
Joshua <154562748+joshuagnes@users.noreply.github.com>
2024-05-31 13:58:25 -0700

    Create script.js

---
+++ ispy/javascript/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const areas = document.querySelectorAll('area');
+  const items = document.querySelectorAll('#items li');
+  let foundItemsCount = 0;
+
+  areas.forEach(area => {
+      area.addEventListener('click', function() {
+          foundItem(area.title);
+      }, false);
+  });
+
+  function foundItem(itemName) {
+      const listItem = Array.from(items).find(item => item.textContent === itemName);
+      if (listItem && !listItem.style.textDecoration) {
+          listItem.style.textDecoration = 'line-through';
+          listItem.style.color = 'green';
+          foundItemsCount++;
+
+          if (foundItemsCount === items.length) {
+              setTimeout(() => {
+                  if (confirm('Congratulations! You found all items. Do you want to play again?')) {
+                      location.reload();
+                  }
+              }, 100);
+          }
+      }
+  }
+});
