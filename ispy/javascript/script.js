6bde83ab5bc87578a6c818b01f65eda52d2099b5
Joshua <154562748+joshuagnes@users.noreply.github.com>
2024-05-31 14:05:52 -0700

    Update script.js

--- ispy/javascript/script.js
+++ ispy/javascript/script.js
@@ -4,25 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
   let foundItemsCount = 0;
 
   areas.forEach(area => {
-      area.addEventListener('click', function() {
-          foundItem(area.title);
-      }, false);
+    area.addEventListener('click', function () {
+      foundItem(area.title);
+    }, false);
   });
 
   function foundItem(itemName) {
-      const listItem = Array.from(items).find(item => item.textContent === itemName);
-      if (listItem && !listItem.style.textDecoration) {
-          listItem.style.textDecoration = 'line-through';
-          listItem.style.color = 'green';
-          foundItemsCount++;
+    const listItem = Array.from(items).find(item => item.textContent === itemName);
+    if (listItem && !listItem.style.textDecoration) {
+      listItem.style.textDecoration = 'line-through';
+      listItem.style.color = 'green';
+      foundItemsCount++;
 
-          if (foundItemsCount === items.length) {
-              setTimeout(() => {
-                  if (confirm('Congratulations! You found all items. Do you want to play again?')) {
-                      location.reload();
-                  }
-              }, 100);
+      if (foundItemsCount === items.length) {
+        setTimeout(() => {
+          if (confirm('Congratulations! You found all items. Do you want to play again?')) {
+            location.reload();
           }
+        }, 100);
       }
+    }
   }
 });
